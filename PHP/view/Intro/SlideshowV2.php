e5d8cd675227fcc513973432877ac56cd7c2be17
un1n3t <matte.mulas@live.it>
2015-04-24 23:27:51 +0200

    verifica bug

--- PHP/view/Intro/SlideshowV2.php
+++ PHP/view/Intro/SlideshowV2.php
@@ -4,7 +4,7 @@
             <ul class="imagesShow">
                 <li><img class="SLDimages"  src="images/Underwather.jpg" alt="1" /></li>
                 <li><img class="SLDimages"  src="images/Watch.jpg" alt="2"/></li>
-				<li><img class="SLDimages"  src="images/watch.jpg" alt="2"/></li>
+				<li><img class="SLDimages"  src="images/watch.jpg" alt="25"/></li>
                 <li><img class="SLDimages"  src="images/woman.jpg" alt="3"/></li>
                 <li><img class="SLDimages"  src="images/teens.jpg" alt="4"/></li>
                 <li><img class="SLDimages"  src="images/morale.jpg" alt="5"/></li>
